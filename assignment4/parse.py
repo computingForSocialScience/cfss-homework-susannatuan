c657b481d54d5eedfd183d5d8dc5c24c607bc40f
Susanna Tuan <sue@Susannas-MacBook-Pro.local>
2015-02-09 23:18:26 -0600

    final homework assignment

--- assignment4/parse.py
+++ assignment4/parse.py
@@ -17,18 +17,28 @@ def get_avg_latlng (data):
 	hppermits=readCSV("permits_hydepark.csv")
 	get_avg_latlng(hppermits)
 
+with open("permits_hydepark.csv") as f:
+	contents = f.readlines(28)
+for line in contents:
+	for c in line.split():
+		self.type = int(c)
+
 def zip_code_barchart():
-	for row in rows:
-		zip_code 
+	fig = plt.figure()
+	axes=fig.add_subplot(111)
+	axes.bar([0.5,1.5,2.5],[4,6,2000])
+	axes.set_title("Hyde Park Zip Code Bar Chart")
+	myrange = range(1,4)
+	axes.set_xticks(myrange)
+	axes.set_xticklabels(['Zip Codes'])
+	axes.set_yticklabels("frequency")
+	plt.draw()
+	plt.savefig('bar.jpg')
+	subprocess.call('open bar.jpg', shell=True)
 
-"""plt.draw()
-plt.savefig('bar.jpg')
-subprocess.call('open bar.jpg', shell=True)"""
 
-fig = plt.figure()
-axes=fig.add_subplot(1,1,1)
-axes.bar([0.5,1.5,2.5],[4,6,2000])
-myrange = range(1,4)
-axes.set_xticks(myrange)
-axes.set_xticklabels(['blah','blah2','blah3'])
-plt.show()
+for arg in sys.argv:
+	if arg == "latlong":
+		print get_avg_latlng("permits_hydepark.csv")
+	elif arg == "hist":
+		print zip_code_barchart("permits_hydepark.csv")
