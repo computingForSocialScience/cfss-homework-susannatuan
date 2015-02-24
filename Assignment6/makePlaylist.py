001e4d03ba90fd7c84e74395dd65eee2b4dd5488
Susanna Tuan <sue@nsit-dhcp-205-208-116-139.uchicago.edu>
2015-02-24 13:08:43 -0600

    Final Assignment

--- Assignment6/makePlaylist.py
+++ Assignment6/makePlaylist.py
@@ -1,15 +1,18 @@
+#number of artist names --> writes csv
 import sys
 import requests
-import csv
-import pandas as pandas
-import networkx as networkx
-import numpy
-from io import open
+import random
 from artistNetworks import *
 from analyzeNetworks import *
 from fetchArtist import *
 from fetchAlbums import *
+import numpy as np
 
+if __name__ == "__main__":
+    artist_names = sys.argv[1:] #listing strings representing arguments on command-line
+    print ('artists are', artist_names)
+
+#now get all of the information from assignment 5 functions
 def fetchArtistId(name):
     """Using the Spotify API search method, take a string that is the artist's name, 
     and return a Spotify artist ID.
@@ -21,8 +24,8 @@ def fetchArtistId(name):
     req = requests.get(url)
     data = req.json()
     artist = data['artists']['items'][0]['id']
-    return artist
-#getting list of artists' ids
+    return np.random.choice.(artist)
+#getting LIST of random artists' ids
 
 def fetchArtistInfo(artist_id):
 	url = "https://api.spotify.com/v1/artists/" + artist_id#get Spotify info about artists that match keyword string
@@ -34,8 +37,8 @@ def fetchArtistInfo(artist_id):
 	info["id"]=data["id"] #string
 	info["name"]=data["name"] #string
 	info["popularity"]=data["popularity"] #int
-	return info
-#getting diictionary of artists' info
+	#return info
+#getting DICTIONARY of artists' info
 
 def fetchAlbumIds(artist_id):
     """Using the Spotify API, take an artist ID and 
@@ -48,8 +51,8 @@ def fetchAlbumIds(artist_id):
     req = requests.get(url)
     data = req.json()
     album = data['items'][0]['id']
-    return album
-#getting list of album ids given the artist's id
+    return np.random.choice(album)
+#getting LIST of random album ids given the artist's id
 
 def fetchAlbumInfo(album_id):
     """Using the Spotify API, take an album ID 
@@ -65,38 +68,38 @@ def fetchAlbumInfo(album_id):
     info["name"]=data["name"] #string
     info["release_date"]=data["release_date"] [:4]
     info["popularity"]=data["popularity"] #int
-    return info
-#getting dictionary of album info
-#need track
+    #return info
+#getting DICTIONARY of album info
+#now, need tracks
 
 def fetchTrack(album_id):
 	req = requests.get("https://api.spotify.com/v1/albums" + album_id + "/tracks")
 	data = req.json()
 	track_list = data['items'][0]['name']
-	return track_list
+	return np.random.choice(track_list) #generates random sample from a given 1-D array
+#getting LIST of random tracks given album id (which is given by artist's id)
 
-if __name__ == "__main__":
-	artist_names = sys.argv[1:] #listing strings representing arguments on command-line
-	def pandasToNetworkX(edgeList):
-		g = nx.DiGraph()
-		for sender, receiver in edgeList.to_records(index = False):
-			g.add_edge(sender, receiver)
-		return(g)
+for variable in artist_names:
+    artist.append(album, track_list)
+    return artist
+#getting new LIST of all random artists, albums and tracks
 
-open('edgeList.csv')
-write.open('edgeList.csv')
+edgeList = getEdgeList(artist_id)
 
+#getting full network list using pandas
+def pandasToNetworkX(edgeList):
+    g = nx.DiGraph()
+    for sender, receiver in edgeList.to_records(index = False):
+        g.add_edge(sender, receiver)
+    return(g)
 
+artists_playlist = []
 
+for artist in artists_playlist:
+    artist_playlists.append(randomCentralNode(g))
+#sampling 30 artists from network using randomCentralNode
 
-
-
-
-
-
-
-
-
+f.write(artists_playlist)
 
 
 
