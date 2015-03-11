00894f54c1974da3ef3d3700092d35c2c9b8891e
Susanna Tuan <sue@wireless-120-58.uchicago.edu>
2015-03-11 18:26:04 -0500

    Most updated version

--- Assignment6/analyzeNetworks.py
+++ Assignment6/analyzeNetworks.py
@@ -1,22 +1,26 @@
+import requests
 import pandas as pd
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
 def readEdgeList(filename):
-	edgeList = pd.read_csv(filename)
-	#pd.read_csv takes a string (URL) and returns a DataFrame
-	dataFrame = pd.DataFrame(edgeList)
-	print dataFrame
+	dataFrame = pd.read_csv(filename) #method of pandas finds the filename
 	if len(dataFrame.columns) > 2:
 		print "Error"
+		dataFrame = pd.read_csv(filename, usecols = [0,1])
+		data = pd.DataFrame(dataFrame)
 	else:
-		dataFrame=dataFrame[dataFrame.columns[0:2]]
-	return dataFrame
+		data=pd.DataFrame(dataFrame)
+	#pd.read_csv takes a string (URL) and returns a DataFrame
+	return data #dataframe
 
-#filename = 'edgeList.csv'
-#readEdgeList(filename)
+filename = 'edgeList.csv' #string
+# print readEdgeList(filename)
+
 #turning the csv file into a DataFrame
+#  artistID           next_artistID
+# 0   2mAFHYBasVVtMekMUkRO9g  6M0icmVnLz7AcdSfpQ1L78
 
 def degree(edgeList, in_or_out):
 	#series.value_counts()  takes a parameter and returns object of counts of values
@@ -32,31 +36,38 @@ def degree(edgeList, in_or_out):
 
 def combineEdgelists(edgeList1, edgeList2):
 	#combining dataframes
-	#dataframe1 = edgeList1
-	#dataframe2 = edgeList2
+	# dataframe1 = edgeList1
+	# dataframe2 = edgeList2
 	concatenated = pd.concat([edgeList1], [edgeList2])
 	return concatenated.drop_duplicates()
-#edgeList1 = readEdgeList('edgeList.csv')
-#edgeList2 = readEdgeList('edgeList.csv')
+edgeList1 = readEdgeList('edgeList.csv')
+edgeList2 = readEdgeList('edgeList.csv')
 #print combineEdgelists(edgeList1, edgeList2)
 
 def pandasToNetworkX(edgeList):
-	g = nx.DiGraph()
-	for sender, receiver in edgeList.to_records(index = False):
-		g.add_edge(sender, receiver)
+	g = nx.DiGraph() #empty graph 
+	for artist, related_artist in edgeList.to_records(index=False): #adds an edge
+		g.add_edge(artist, related_artist)
+	#print g.number_of_edges()
 	return(g)
 
-#edgeList = 'edgeList.csv'
-#print pandasToNetworkX(edgeList)
+d=readEdgeList('edgeList.csv')
+# g = pandasToNetworkX(d)
+# print g.nodes()
 
 def randomCentralNode(inputDiGraph):
 	#takes graph --> single node from that network
-	nodes_dict = nx.eigenvector_centrality(pandasToNetworkX(edgeList))
+	nodes_dict = nx.eigenvector_centrality(inputDiGraph) #network method assigns centrality value to each node
+	#keys are artist ids, value is centrality(weights)
+	total = sum(nodes_dict.values()) #total is 0
 	#nx.eigenvector_centrality() takes graph as parameter and returns nodes in a dict.
-	#print nx.eigenvector_centrality(pandasToNetworkX(edgeList))
-	normal_dict = sum(nodes_dict.values())
-	#probabilities do not sum to 1?!?
-	random_node = np.random.choice(nodes_dict.keys(), p = nodes_dict.values())
+	for variable in nodes_dict:
+		if total == 0:
+			nodes_dict[variable]=1.0/len(inputDiGraph)
+		else:
+			nodes_dict[variable]=nodes_dict[variable]/float(total)
+
+	random_node = np.random.choice(nodes_dict.keys(), p = nodes_dict.values()) #need to sum to 1 because of probability rules
 	return random_node
 
 edgeList = readEdgeList('edgeList.csv')
