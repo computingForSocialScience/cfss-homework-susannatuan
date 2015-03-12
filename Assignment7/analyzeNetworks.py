6313e47512415ad6c47c3bd26f9c78a5ee150689
Susanna Tuan <sue@wireless-120-58.uchicago.edu>
2015-03-11 23:03:30 -0500

    Fixed pandasToNetworkX

--- Assignment7/analyzeNetworks.py
+++ Assignment7/analyzeNetworks.py
@@ -5,7 +5,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 def readEdgeList(filename):
-	dataFrame = pd.read_csv(filename)
+	dataFrame = pd.read_csv(filename) #method of pandas finds the filename
 	if len(dataFrame.columns) > 2:
 		print "Error"
 		dataFrame = pd.read_csv(filename, usecols = [0,1])
@@ -13,9 +13,9 @@ def readEdgeList(filename):
 	else:
 		data=pd.DataFrame(dataFrame)
 	#pd.read_csv takes a string (URL) and returns a DataFrame
-	return data
+	return data #dataframe
 
-# filename = 'edgeList.csv'
+filename = 'edgeList.csv' #string
 # print readEdgeList(filename)
 
 #turning the csv file into a DataFrame
@@ -45,23 +45,29 @@ edgeList2 = readEdgeList('edgeList.csv')
 #print combineEdgelists(edgeList1, edgeList2)
 
 def pandasToNetworkX(edgeList):
-	g = nx.DiGraph()
-	for artist, related_artist in edgeList.to_records(index=False):
+	g = nx.DiGraph() #empty graph 
+	for artist, related_artist in edgeList.to_records(index=False): #adds an edge
 		g.add_edge(artist, related_artist)
+	#print g.number_of_edges()
 	return(g)
 
-edgeList = 'edgeList.csv'
-#print pandasToNetworkX(edgeList)
+d=readEdgeList('edgeList.csv')
+# g = pandasToNetworkX(d)
+# print g.nodes()
 
 def randomCentralNode(inputDiGraph):
 	#takes graph --> single node from that network
-	nodes_dict = nx.eigenvector_centrality(pandasToNetworkX(inputDiGraph))
-	total = sum(nodes_dict.values())
+	nodes_dict = nx.eigenvector_centrality(inputDiGraph) #network method assigns centrality value to each node
+	#keys are artist ids, value is centrality(weights)
+	total = sum(nodes_dict.values()) #total is 0
 	#nx.eigenvector_centrality() takes graph as parameter and returns nodes in a dict.
 	for variable in nodes_dict:
-		nodes_dict[variable]=nodes_dict[variable]/float(total)
+		if total == 0:
+			nodes_dict[variable]=1.0/len(inputDiGraph)
+		else:
+			nodes_dict[variable]=nodes_dict[variable]/float(total)
 
-	random_node = np.random.choice(nodes_dict.keys(), p = nodes_dict.values())
+	random_node = np.random.choice(nodes_dict.keys(), p = nodes_dict.values()) #need to sum to 1 because of probability rules
 	return random_node
 
 edgeList = readEdgeList('edgeList.csv')
